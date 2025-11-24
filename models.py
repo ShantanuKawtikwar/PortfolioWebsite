a6709991954e6b5e614785dee0f887b828860cfe
shantanu kawtikwar <159459525+ShantanuKawtikwar@users.noreply.github.com>
2025-11-24 07:16:27 +0530

    Update string length for model fields and add image

--- models.py
+++ models.py
@@ -7,16 +7,18 @@ class Project(db.Model):
     title = db.Column(db.String(200))
     description = db.Column(db.Text)
     date = db.Column(db.String(50))
+    image = db.Column(db.String(300))
 
 class Certification(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250))
-    issuer = db.Column(db.String(150))
+    title = db.Column(db.String(200))
+    issuer = db.Column(db.String(200))
     date = db.Column(db.String(50))
+    image = db.Column(db.String(300))
 
 class Timeline(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     month = db.Column(db.String(50))
     title = db.Column(db.String(200))
-    organization = db.Column(db.String(150))
+    organization = db.Column(db.String(200))
     details = db.Column(db.Text)
